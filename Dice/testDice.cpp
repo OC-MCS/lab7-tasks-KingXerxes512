91c7631b31afe0e9d8a865c3321696569cbe4359
Luke Rodman <luke.rodman@eagles.oc.edu>
2019-02-22 09:51:10 -0600

    done with dice

--- Dice/testDice.cpp
+++ Dice/testDice.cpp
@@ -1,8 +1,44 @@
+#include "Die.h"
+#include <iostream>
+#include <iomanip>
 
-// client code to use the Die class goes here
+using namespace std;
 
 int main()
 {
+	Die SixSided(6);
+	Die TwelveSided(12);
+	int sixArray[7] = { 0, 0, 0, 0, 0, 0, 0 };
+	int twelveArray[13] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	for (int counter = 0; counter < 1000000; counter++)
+	{
+		SixSided.roll();
+		TwelveSided.roll();
+		for (int index = 1; index < 7; index++)
+		{
+			if (SixSided.getValue() == index)
+			{
+				sixArray[index]++;
+			}
+		}
+		for (int index = 1; index < 13; index++)
+		{
+			if (TwelveSided.getValue() == index)
+			{
+				twelveArray[index]++;
+			}
+		}
+	}
+	cout << "Stats for Six Sided Die: " << endl;
+	for (int counter = 1; counter < 7; counter++)
+	{
+		cout << "Side " << counter << ": " << fixed << setprecision(2) << (sixArray[counter] / 10000.0) << "%" << endl;
+	}
+	cout << "\nStats for the Twelve Sided Die: " << endl;
+	for (int counter = 1; counter < 13; counter++)
+	{
+		cout << "Side " << counter << ": " << fixed << setprecision(2) << (twelveArray[counter] / 10000.0) << "%" << endl;
+	}
 
-	return 0;
+	cout << endl; system("pause"); return 0;
 }
