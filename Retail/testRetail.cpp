a0a560fb8c86a31cb07e52efd59be2dedda7e978
Luke Rodman <luke.rodman@eagles.oc.edu>
2019-02-27 13:27:45 -0600

    Final Ver.

--- Retail/testRetail.cpp
+++ Retail/testRetail.cpp
@@ -1,5 +1,6 @@
 #include "RetailItem.h"
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
 #include <fstream>
@@ -8,14 +9,24 @@ void loadItems(vector<RetailItem> &object);
 
 int main()
 {
+	double total = 0;
 	vector<RetailItem> object;
 	loadItems(object);
-	cout << (object[0]).getDescription() << endl;
-	cout << (object[1]).getDescription() << endl;
-	cout << (object[2]).getDescription() << endl;
 
-	//cout << "\t Description \t Price \t On Hand \t Value\n";
-	
+	// Calculates the total value for entire inventory
+	for (int counter = 0; counter < object.size() - 1; counter++)
+	{
+		total += object[counter].getStockValue();
+	}
+
+	// Prints a detailed report of what is in inventory
+	cout << "\t Description \t Price \t On Hand \t Value\n";
+	for (int counter = 0; counter < object.size() - 1; counter++)
+	{
+		cout << setw(25) << left << object[counter].getDescription() << object[counter].getPrice() << setw(8) << right << object[counter].getUnitsOnHand() <<
+			setw(16) << object[counter].getStockValue() << endl;
+	}
+	cout << "\nTotal Inventory Value: " << setprecision(2) << fixed << total << endl;
 
 	return 0;
 }
@@ -31,16 +42,16 @@ void loadItems(vector<RetailItem> &object)
 	RetailItem buffer("null", 0, 0);
 	
 	ifstream data;
-	data.open("Lab7 Data.csv");
-	if (!data)
+	data.open("RetailData.csv");
+	if (!data)	// Checks to see if file was opened or not
 	{
-		cout << "Could not open data file\n";
+		cout << "Could not open RetailData.csv\n";
 		exit(EXIT_FAILURE);
 	}
 	else
-	{
+	{	// Runs loop which continually sets the values of the buffer object and then back adds the object to the Vector object, until eof
 		data.seekg(0);
-		while (data.good())
+		while (!data.eof())
 		{
 			getline(data, description, ',');
 			getline(data, tempUnits, ',');
